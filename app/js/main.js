3b5d496865a7447de4645d550ce3acd44414e672
Tatiana-KTV <tatiana.cosacenco@gmail.com>
2024-07-23 13:29:26 +0200

    added testimonials section, added slick slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,10 @@ $(document).scroll(function (e) {
     $('.header__top').addClass('header-fixed') :
     $('.header__top').removeClass('header-fixed');
 });
+$('.slider').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  focusOnSelect: true,
+});
 });
