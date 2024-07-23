8d86cde39f97b6b406605f669d01fca596c2a496
Tatiana-KTV <tatiana.cosacenco@gmail.com>
2024-07-23 14:24:40 +0200

    added styles to slick-slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,13 @@ $(document).scroll(function (e) {
 });
 $('.slider').slick({
   dots: true,
+  focusOnSelect: true,
   arrows: true,
   infinite: false,
-  focusOnSelect: true,
+  appendDots: '.slider-navigation__dots',
+  // appendDots: '.slider-navigation',
+  appendArrows: '.slider-navigation',
+  nextArrow: '<button type="button" class="slider-navigation__arrow slider-navigation__arrow--prev"><svg class="slider-arrow__arrow-left" fill="#000"><use xlink:href="images/sprite.svg#icon-prev-arrow"></use></svg></button>',
+  prevArrow: '<button type="button" class="slider-navigation__arrow slider-navigation__arrow--next"><svg class="slider-arrow__arrow-right" fill="#000"><use xlink:href="images/sprite.svg#icon-prev-arrow"></use></svg></button>',
 });
-});
+}); 
